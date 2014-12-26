7f1692f9d46d6be35fcc4715b50306caee2a70e2
Anton Egorov <anton.egoroff@gmail.com>
2014-12-26 22:16:42 +0300

    setup.py with manifest

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+from cmsplugin_iframe import __version__
+
+
+CLASSIFIERS = [
+    'Development Status :: 1 - Planning',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Communications',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Message Boards',
+    'Topic :: Internet :: WWW/HTTP :: Site Management',
+    'Programming Language :: Python :: 2.7',
+]
+
+setup(
+    name='cmsplugin-iframe',
+    version=__version__,
+    description='Django CMS page iframe plugin',
+    author='Anton Egorov',
+    author_email='anton.egoroff@gmail.com',
+    url='https://github.com/satyrius/cmsplugin-iframe',
+    license='MIT',
+    long_description=open('README.rst').read(),
+    classifiers=CLASSIFIERS,
+    platforms=['OS Independent'],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'django-cms',
+    ],
+    tests_require=['tox>=1.8'],
+    zip_safe=False,
+)
