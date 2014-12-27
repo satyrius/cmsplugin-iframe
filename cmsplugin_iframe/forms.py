b4f81e437fb610110d7170e29fb63df480688c04
Anton Egorov <anton.egoroff@gmail.com>
2014-12-27 18:46:23 +0300

    custom url field to extract src from iframe snippet

--- cmsplugin_iframe/forms.py
+++ cmsplugin_iframe/forms.py
@@ -1,10 +1,28 @@
+import re
+
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 from .models import IframePlugin
 
 
+class EmbedURLField(forms.URLField):
+    widget = forms.Textarea
+
+    def clean(self, value):
+        if re.search(r'<iframe', value, re.IGNORECASE):
+            m = re.search(r'src="([^"]+)"', value)
+            if not m:
+                raise forms.ValidationError(
+                    _('The `iframe` HTML snippet was passed, but there is no '
+                      '`src` arrtibute was found'),
+                    code='invalid')
+            value = m.group(1)
+        return super(EmbedURLField, self).clean(value)
+
+
 class IframeForm(forms.ModelForm):
-    src = forms.URLField(widget=forms.Textarea)
+    src = EmbedURLField()
 
     class Meta:
         model = IframePlugin
--- tests/test_form.py
+++ tests/test_form.py
@@ -9,3 +9,13 @@ class IframeFormTest(unittest.TestCase):
     def test_url_as_is(self):
         form = IframeForm(data={'src': self.url})
         self.assertTrue(form.is_valid(), form.errors)
+        self.assertEqual(form.cleaned_data['src'], self.url)
+
+    def test_extract_from_embed_code(self):
+        emded_code = '''
+            <iframe width="560" height="315" src="{url}"
+                frameborder="0" allowfullscreen></iframe>
+        '''.format(url=self.url).strip()
+        form = IframeForm(data={'src': emded_code})
+        self.assertTrue(form.is_valid(), form.errors)
+        self.assertEqual(form.cleaned_data['src'], self.url)
